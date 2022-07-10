c57cced7f9232654dd909b885b8c989f157c642d
Rodrigo RB <rodrigo.rb.dev@gmail.com>
2022-07-10 12:06:59 -0300

    feat: add base Block class

---
+++ main.js
@@ -0,0 +1,20 @@
+import sha256 from 'crypto-js/sha256';
+
+class Block {
+  constructor(
+    index,
+    timestamp,
+    data,
+    previousHash = ''
+  ) {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = this.calculateHash();
+  }
+
+  calculateHash() {
+    return sha256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data));
+  }
+}
