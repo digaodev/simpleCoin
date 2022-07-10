4bc42a8df4fe131f507b333cc10a2c3509eee3ad
Rodrigo RB <rodrigo.rb.dev@gmail.com>
2022-07-10 12:28:13 -0300

    feat: add Blockchain class and basic functions to add a new block

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import sha256 from 'crypto-js/sha256';
+var SHA256 = require("crypto-js/sha256");
 
 class Block {
   constructor(
@@ -15,6 +15,32 @@ class Block {
   }
 
   calculateHash() {
-    return sha256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data));
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
   }
 }
+
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+  }
+
+  createGenesisBlock() {
+    return new Block(0, '01/01/2022', 'Genesis block', null);
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+}
+
+let simpleCoin = new Blockchain();
+simpleCoin.addBlock(new Block(1, '10/01/2022', { amount: 4 }));
+simpleCoin.addBlock(new Block(1, '20/01/2022', { amount: 4 }));
+
+console.log(JSON.stringify(simpleCoin, null, 4));
